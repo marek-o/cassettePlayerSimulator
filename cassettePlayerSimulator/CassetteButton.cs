519de23713dbaa9f29e19b4d28474c7649f00faa
Marek Ossowski <marek0ossowski@gmail.com>
2020-09-17 20:32:10 +0200

    Handling of pressing a button

--- cassettePlayerSimulator/CassetteButton.cs
+++ cassettePlayerSimulator/CassetteButton.cs
@@ -19,21 +19,51 @@ namespace cassettePlayerSimulator
             InitializeComponent();
         }
 
-        private int frame = 0;
+        public enum State
+        {
+            UP, //pause when paused
+            PRESSED_UP_DOWN, //pause when pausing
+            DOWN, //pause when unpaused
+            PRESSED_DOWN_UP //pause when unpausing
+        }
+
+        public enum Type
+        {
+            NOT_LOCKING, //stop/eject
+            LOCKING, //play
+            BISTABLE, //pause
+        }
+
+        public State ButtonState { get; set; } = State.UP;
+        public Type ButtonType { get; set; } = Type.BISTABLE;
 
         protected override void OnPaint(PaintEventArgs e)
         {
             base.OnPaint(e);
 
-            var brush = ((frame % 2) == 0) ? Brushes.Red : Brushes.Yellow;
-            e.Graphics.FillRectangle(brush, new RectangleF(0, 0, Width, Height));
+            Brush b = Brushes.Red;
+
+            switch (ButtonState)
+            {
+                case State.UP: b = Brushes.LightGray; break;
+                case State.PRESSED_UP_DOWN: b = Brushes.DarkRed; break;
+                case State.DOWN: b = Brushes.Gray; break;
+                case State.PRESSED_DOWN_UP: b = Brushes.DarkOrange; break;
+            }
+
+            e.Graphics.FillRectangle(b, new RectangleF(0, 0, Width, Height));
         }
 
         protected override void OnMouseDown(MouseEventArgs e)
         {
             base.OnMouseDown(e);
 
-            frame++;
+            switch (ButtonState)
+            {
+                case State.UP: ButtonState = State.PRESSED_UP_DOWN; break;
+                case State.DOWN: ButtonState = State.PRESSED_DOWN_UP; break;
+            }
+
             Invalidate();
         }
 
@@ -41,7 +71,31 @@ namespace cassettePlayerSimulator
         {
             base.OnMouseUp(e);
 
-            frame++;
+            if (ButtonType == Type.NOT_LOCKING)
+            {
+                switch (ButtonState)
+                {
+                    case State.PRESSED_UP_DOWN: ButtonState = State.UP; break;
+                    case State.PRESSED_DOWN_UP: ButtonState = State.DOWN; break;
+                }
+            }
+            else if (ButtonType == Type.LOCKING)
+            {
+                switch (ButtonState)
+                {
+                    case State.PRESSED_UP_DOWN: ButtonState = State.DOWN; break;
+                    case State.PRESSED_DOWN_UP: ButtonState = State.DOWN; break;
+                }
+            }
+            else if (ButtonType == Type.BISTABLE)
+            {
+                switch (ButtonState)
+                {
+                    case State.PRESSED_UP_DOWN: ButtonState = State.DOWN; break;
+                    case State.PRESSED_DOWN_UP: ButtonState = State.UP; break;
+                }
+            }
+
             Invalidate();
         }
     }

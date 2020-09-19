1634b262a7ec94f460a930c68238222623211896
Marek Ossowski <marek0ossowski@gmail.com>
2020-09-19 21:33:38 +0200

    Experimenting with button graphics

--- cassettePlayerSimulator/Form1.Designer.cs
+++ cassettePlayerSimulator/Form1.Designer.cs
@@ -32,14 +32,19 @@
             this.buttonImport = new System.Windows.Forms.Button();
             this.buttonLoadTape = new System.Windows.Forms.Button();
             this.panel2 = new System.Windows.Forms.Panel();
-            this.cassetteControl1 = new cassettePlayerSimulator.CassetteControl();
+            this.cassetteButton4 = new cassettePlayerSimulator.CassetteButton();
+            this.cassetteButton5 = new cassettePlayerSimulator.CassetteButton();
+            this.cassetteButton6 = new cassettePlayerSimulator.CassetteButton();
+            this.cassetteButton2 = new cassettePlayerSimulator.CassetteButton();
             this.cassetteButton1 = new cassettePlayerSimulator.CassetteButton();
+            this.cassetteButton3 = new cassettePlayerSimulator.CassetteButton();
+            this.cassetteControl1 = new cassettePlayerSimulator.CassetteControl();
             this.SuspendLayout();
             // 
             // panel1
             // 
             this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
-            this.panel1.Location = new System.Drawing.Point(12, 285);
+            this.panel1.Location = new System.Drawing.Point(504, 50);
             this.panel1.Name = "panel1";
             this.panel1.Size = new System.Drawing.Size(93, 92);
             this.panel1.TabIndex = 0;
@@ -48,7 +53,7 @@
             // 
             // buttonImport
             // 
-            this.buttonImport.Location = new System.Drawing.Point(12, 383);
+            this.buttonImport.Location = new System.Drawing.Point(504, 148);
             this.buttonImport.Name = "buttonImport";
             this.buttonImport.Size = new System.Drawing.Size(75, 23);
             this.buttonImport.TabIndex = 1;
@@ -58,7 +63,7 @@
             // 
             // buttonLoadTape
             // 
-            this.buttonLoadTape.Location = new System.Drawing.Point(12, 412);
+            this.buttonLoadTape.Location = new System.Drawing.Point(504, 177);
             this.buttonLoadTape.Name = "buttonLoadTape";
             this.buttonLoadTape.Size = new System.Drawing.Size(75, 23);
             this.buttonLoadTape.TabIndex = 2;
@@ -69,36 +74,87 @@
             // panel2
             // 
             this.panel2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
-            this.panel2.Location = new System.Drawing.Point(111, 285);
+            this.panel2.Location = new System.Drawing.Point(603, 50);
             this.panel2.Name = "panel2";
             this.panel2.Size = new System.Drawing.Size(93, 92);
             this.panel2.TabIndex = 3;
             this.panel2.MouseDown += new System.Windows.Forms.MouseEventHandler(this.panel2_MouseDown);
             this.panel2.MouseUp += new System.Windows.Forms.MouseEventHandler(this.panel2_MouseUp);
             // 
-            // cassetteControl1
+            // cassetteButton4
             // 
-            this.cassetteControl1.Location = new System.Drawing.Point(12, 12);
-            this.cassetteControl1.Name = "cassetteControl1";
-            this.cassetteControl1.Size = new System.Drawing.Size(422, 267);
-            this.cassetteControl1.TabIndex = 5;
+            this.cassetteButton4.ButtonState = cassettePlayerSimulator.CassetteButton.State.UP;
+            this.cassetteButton4.ButtonType = cassettePlayerSimulator.CassetteButton.Type.BISTABLE;
+            this.cassetteButton4.Location = new System.Drawing.Point(397, 328);
+            this.cassetteButton4.Name = "cassetteButton4";
+            this.cassetteButton4.Size = new System.Drawing.Size(80, 81);
+            this.cassetteButton4.TabIndex = 12;
+            // 
+            // cassetteButton5
+            // 
+            this.cassetteButton5.ButtonState = cassettePlayerSimulator.CassetteButton.State.UP;
+            this.cassetteButton5.ButtonType = cassettePlayerSimulator.CassetteButton.Type.BISTABLE;
+            this.cassetteButton5.Location = new System.Drawing.Point(320, 328);
+            this.cassetteButton5.Name = "cassetteButton5";
+            this.cassetteButton5.Size = new System.Drawing.Size(80, 81);
+            this.cassetteButton5.TabIndex = 11;
+            // 
+            // cassetteButton6
+            // 
+            this.cassetteButton6.ButtonState = cassettePlayerSimulator.CassetteButton.State.UP;
+            this.cassetteButton6.ButtonType = cassettePlayerSimulator.CassetteButton.Type.BISTABLE;
+            this.cassetteButton6.Location = new System.Drawing.Point(243, 328);
+            this.cassetteButton6.Name = "cassetteButton6";
+            this.cassetteButton6.Size = new System.Drawing.Size(80, 81);
+            this.cassetteButton6.TabIndex = 10;
+            // 
+            // cassetteButton2
+            // 
+            this.cassetteButton2.ButtonState = cassettePlayerSimulator.CassetteButton.State.UP;
+            this.cassetteButton2.ButtonType = cassettePlayerSimulator.CassetteButton.Type.BISTABLE;
+            this.cassetteButton2.Location = new System.Drawing.Point(166, 328);
+            this.cassetteButton2.Name = "cassetteButton2";
+            this.cassetteButton2.Size = new System.Drawing.Size(80, 81);
+            this.cassetteButton2.TabIndex = 9;
             // 
             // cassetteButton1
             // 
             this.cassetteButton1.ButtonState = cassettePlayerSimulator.CassetteButton.State.UP;
             this.cassetteButton1.ButtonType = cassettePlayerSimulator.CassetteButton.Type.BISTABLE;
-            this.cassetteButton1.Location = new System.Drawing.Point(210, 285);
+            this.cassetteButton1.Location = new System.Drawing.Point(89, 328);
             this.cassetteButton1.Name = "cassetteButton1";
-            this.cassetteButton1.Size = new System.Drawing.Size(98, 92);
-            this.cassetteButton1.TabIndex = 4;
+            this.cassetteButton1.Size = new System.Drawing.Size(80, 81);
+            this.cassetteButton1.TabIndex = 8;
+            // 
+            // cassetteButton3
+            // 
+            this.cassetteButton3.ButtonState = cassettePlayerSimulator.CassetteButton.State.UP;
+            this.cassetteButton3.ButtonType = cassettePlayerSimulator.CassetteButton.Type.BISTABLE;
+            this.cassetteButton3.Location = new System.Drawing.Point(12, 328);
+            this.cassetteButton3.Name = "cassetteButton3";
+            this.cassetteButton3.Size = new System.Drawing.Size(80, 81);
+            this.cassetteButton3.TabIndex = 7;
+            // 
+            // cassetteControl1
+            // 
+            this.cassetteControl1.Location = new System.Drawing.Point(39, 12);
+            this.cassetteControl1.Name = "cassetteControl1";
+            this.cassetteControl1.Size = new System.Drawing.Size(422, 267);
+            this.cassetteControl1.TabIndex = 5;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.cassetteControl1);
+            this.Controls.Add(this.cassetteButton4);
+            this.Controls.Add(this.cassetteButton5);
+            this.Controls.Add(this.cassetteButton6);
+            this.Controls.Add(this.cassetteButton2);
             this.Controls.Add(this.cassetteButton1);
+            this.Controls.Add(this.cassetteButton3);
+            this.Controls.Add(this.cassetteControl1);
             this.Controls.Add(this.panel2);
             this.Controls.Add(this.buttonLoadTape);
             this.Controls.Add(this.buttonImport);
@@ -115,8 +171,13 @@
         private System.Windows.Forms.Button buttonImport;
         private System.Windows.Forms.Button buttonLoadTape;
         private System.Windows.Forms.Panel panel2;
-        private CassetteButton cassetteButton1;
         private CassetteControl cassetteControl1;
+        private CassetteButton cassetteButton3;
+        private CassetteButton cassetteButton1;
+        private CassetteButton cassetteButton2;
+        private CassetteButton cassetteButton4;
+        private CassetteButton cassetteButton5;
+        private CassetteButton cassetteButton6;
     }
 }
 

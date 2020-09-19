157593a7aa994921c0d0c44fd20a0c1306e28d19
Marek Ossowski <marek0ossowski@gmail.com>
2020-09-19 17:08:49 +0200

    Added some public domain cassette image

--- cassettePlayerSimulator/Properties/Resources.Designer.cs
+++ cassettePlayerSimulator/Properties/Resources.Designer.cs
@@ -109,6 +109,16 @@ namespace cassettePlayerSimulator.Properties {
         }
         
         /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap cassette {
+            get {
+                object obj = ResourceManager.GetObject("cassette", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
         ///   Looks up a localized resource of type System.IO.UnmanagedMemoryStream similar to System.IO.MemoryStream.
         /// </summary>
         internal static System.IO.UnmanagedMemoryStream ejectDown {

56e82b87be0567bb2fb97adec77c53233f300f3d
Marek Ossowski <marek0ossowski@gmail.com>
2020-09-26 15:11:50 +0200

    Drawing tape spools

--- cassettePlayerSimulator/CassetteControl.cs
+++ cassettePlayerSimulator/CassetteControl.cs
@@ -19,19 +19,70 @@ namespace cassettePlayerSimulator
             InitializeComponent();
         }
 
+        private Brush tapeBrush = new SolidBrush(Color.FromArgb(128, 64, 0));
+        private Brush spoolBrush = new SolidBrush(Color.FromArgb(240, 240, 240));
+
+        private Brush backgroundBrush = new SolidBrush(Color.FromArgb(128, 128, 128));
+
+        private float scale;
+
         protected override void OnPaint(PaintEventArgs e)
         {
             base.OnPaint(e);
 
             var img = Properties.Resources.cassette;
-            RectangleF destRect = new RectangleF(0, 0, img.Width * Width / img.Width, img.Height * Width / img.Width);
+            scale = (float) Width / img.Width;
 
-            if (destRect.Height > Height)
+            if (img.Height * scale > Height)
             {
-                destRect = new RectangleF(0, 0, img.Width * Height / img.Height, img.Height * Height / img.Height);
+                scale = (float) Height / img.Height;
             }
 
+            RectangleF destRect = new RectangleF(0, 0, img.Width * scale, img.Height * scale);
+
+            PointF centerLeft = new PointF(371 * scale, 377 * scale);
+            PointF centerRight = new PointF(899 * scale, 377 * scale);
+
+            DrawTapeSpool(e.Graphics, centerLeft, 295 * scale);
+            DrawTapeSpool(e.Graphics, centerRight, 135 * scale);
+
             e.Graphics.DrawImage(img, destRect, new RectangleF(0, 0, img.Width, img.Height), GraphicsUnit.Pixel);
+        }
+
+        private PointF PolarToCartesian(PointF center, float r, float a)
+        {
+            return new PointF
+            (
+                center.X + r * (float)Math.Cos(a * Math.PI / 180),
+                center.Y + r * (float)Math.Sin(a * Math.PI / 180)
+            );
+        }
+
+        private void DrawTapeSpool(Graphics g, PointF center, float outerRadius)
+        {
+            float spoolInnerRadius = 67 * scale;
+            float spoolMinRadius = 135 * scale;
+            float spoolMaxRadius = 295 * scale;
+
+            g.FillEllipse(tapeBrush, center.X - outerRadius, center.Y - outerRadius,
+                outerRadius * 2, outerRadius * 2);
+            g.FillEllipse(spoolBrush, center.X - spoolMinRadius, center.Y - spoolMinRadius,
+                spoolMinRadius * 2, spoolMinRadius * 2);
+            g.FillEllipse(backgroundBrush, center.X - spoolInnerRadius, center.Y - spoolInnerRadius,
+                spoolInnerRadius * 2, spoolInnerRadius * 2);
+
+            for (int i = 0; i < 6; ++i)
+            {
+                float angle = 60 * i;
+
+                g.FillPolygon(spoolBrush, new PointF[]
+                {
+                    PolarToCartesian(center, 55 * scale, angle),
+                    PolarToCartesian(center, 55 * scale, angle + 20),
+                    PolarToCartesian(center, 80 * scale, angle + 20),
+                    PolarToCartesian(center, 80 * scale, angle),
+                });
+            }
         }
     }
 }

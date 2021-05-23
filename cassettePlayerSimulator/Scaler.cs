e5e0b22fac405daa75dffe437231764ff2de969a
Marek Ossowski <marek0ossowski@gmail.com>
2021-05-23 16:55:53 +0200

    Simplifying Scaler, setting scale once per object, shortened method name, extracted DPI scaling to DpiScaler

--- cassettePlayerSimulator/Scaler.cs
+++ cassettePlayerSimulator/Scaler.cs
@@ -7,9 +7,54 @@ using System.Drawing;
 
 namespace cassettePlayerSimulator
 {
-    class Scaler
+    public class Scaler
     {
-        static Scaler()
+        public Scaler(float factor = 1.0f)
+        {
+            scalingFactor = factor;
+        }
+
+        private float scalingFactor = 1.0f;
+
+        public float ScalingFactor
+        {
+            get
+            {
+                return scalingFactor;
+            }
+            set
+            {
+                scalingFactor = value;
+            }
+        }
+
+        public float S(float input)
+        {
+            return input * scalingFactor;
+        }
+
+        public int S(int input)
+        {
+            return (int)(input * scalingFactor);
+        }
+
+        public Rectangle S(Rectangle input)
+        {
+            return new Rectangle(
+                S(input.X),
+                S(input.Y),
+                S(input.Width),
+                S(input.Height));
+        }
+    }
+
+    public class DpiScaler : Scaler
+    {
+        private static float dpiScaleFactor = 1.0f;
+
+        public static float DpiScaleFactor => dpiScaleFactor;
+
+        static DpiScaler()
         {
             using (var bmp = new Bitmap(1, 1))
             {
@@ -20,28 +65,9 @@ namespace cassettePlayerSimulator
             }
         }
 
-        private static float dpiScaleFactor = 1.0f;
-
-        public static float DpiScaleFactor => dpiScaleFactor;
-
-        public static float Scale(float input, float factor)
+        public DpiScaler(): base(dpiScaleFactor)
         {
-            return input * factor;
-        }
 
-        public static int Scale(int input, float factor)
-        {
-            return (int)(input * factor);
-        }
-
-        public static float DpiScale(float input)
-        {
-            return Scale(input, dpiScaleFactor);
-        }
-
-        public static int DpiScale(int input)
-        {
-            return Scale(input, dpiScaleFactor);
         }
     }
 }

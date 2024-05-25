a7e77e4577d14f670a47518efef5083485d6bb99
Marek Ossowski <marek0ossowski@gmail.com>
2024-05-25 15:41:03 +0200

    Removed unused images

--- cassettePlayerSimulator/Properties/Resources.Designer.cs
+++ cassettePlayerSimulator/Properties/Resources.Designer.cs
@@ -19,7 +19,7 @@ namespace cassettePlayerSimulator.Properties {
     // class via a tool like ResGen or Visual Studio.
     // To add or remove a member, edit your .ResX file then rerun ResGen
     // with the /str option, or rebuild your VS project.
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "16.0.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "17.0.0.0")]
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
     internal class Resources {
@@ -57,36 +57,6 @@ namespace cassettePlayerSimulator.Properties {
             }
             set {
                 resourceCulture = value;
-            }
-        }
-        
-        /// <summary>
-        ///   Looks up a localized resource of type System.Drawing.Bitmap.
-        /// </summary>
-        internal static System.Drawing.Bitmap buttonDown {
-            get {
-                object obj = ResourceManager.GetObject("buttonDown", resourceCulture);
-                return ((System.Drawing.Bitmap)(obj));
-            }
-        }
-        
-        /// <summary>
-        ///   Looks up a localized resource of type System.Drawing.Bitmap.
-        /// </summary>
-        internal static System.Drawing.Bitmap buttonPressed {
-            get {
-                object obj = ResourceManager.GetObject("buttonPressed", resourceCulture);
-                return ((System.Drawing.Bitmap)(obj));
-            }
-        }
-        
-        /// <summary>
-        ///   Looks up a localized resource of type System.Drawing.Bitmap.
-        /// </summary>
-        internal static System.Drawing.Bitmap buttonUp {
-            get {
-                object obj = ResourceManager.GetObject("buttonUp", resourceCulture);
-                return ((System.Drawing.Bitmap)(obj));
             }
         }
         

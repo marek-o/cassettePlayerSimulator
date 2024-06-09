79798a7bf316762c90e5803cdbd2f33af4310878
Marek Ossowski <marek0ossowski@gmail.com>
2024-06-09 19:52:42 +0200

    Added NGettext, added sketch of translations

--- cassettePlayerSimulator/Program.cs
+++ cassettePlayerSimulator/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -8,12 +9,17 @@ namespace cassettePlayerSimulator
 {
     static class Program
     {
+        internal static NGettext.Catalog tr;
+
         /// <summary>
         /// The main entry point for the application.
         /// </summary>
         [STAThread]
         static void Main()
         {
+            tr = new NGettext.Catalog(File.Open("../../../translations/pl.mo", FileMode.Open));
+            MessageBox.Show(tr.GetString("hello"));
+
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new SimulatorForm());
